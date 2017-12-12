1c69311422b1558087d630adbc0699d6add511fd
Fiach Reid <fiach.reid@gmail.com>
2017-12-12 15:18:50 +0000

    initial Commit

---
+++ tableOnly.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tableOnly.aspx.cs" Inherits="MobileSQL.tableOnly" %>
+<html>
+<asp:DataGrid id="dgData" Width="100%" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+    <AlternatingItemStyle BackColor="White" ForeColor="#284775" />
+    <EditItemStyle BackColor="#999999" />
+    <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+    <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+    <ItemStyle BackColor="#F7F6F3" ForeColor="#333333" />
+    <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+    <SelectedItemStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+</asp:DataGrid>
+
+<asp:label id="lblError" runat="server"  BorderColor="Red"></asp:label>
+
+</html>
---
+++ tableOnly.aspx.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MobileSQL
+{
+    public partial class tableOnly : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                var strDataSource = Request.QueryString["DataSource"];
+                var strUsername = Request.QueryString["Username"];
+                var strPassword = Request.QueryString["Password"];
+                var strSQL = Request.QueryString["SQL"];
+                var strDB = Request.QueryString["DB"].ToLower();
+                if (strDB == "sql")
+                {
+                    ExecuteMSSQL(strDataSource, strUsername, strPassword, strSQL);
+                }
+                else
+                {
+                    ExecuteMYSQL(strDataSource, strUsername, strPassword, strSQL);
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+        }
+
+        private void ExecuteMSSQL(string strDataSource, string strUsername, string strPassword, string strSQL)
+        {
+            var strDSNTemplate = "Network Library=DBMSSOCN;Data Source={0}; User ID={1}; Password={2};";
+            var sqlConnection = string.Format(strDSNTemplate, strDataSource, strUsername, strPassword);
+            var adapter = new SqlDataAdapter(strSQL, sqlConnection);
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet, "sql");
+            var dt = dataSet.Tables["sql"];
+            this.dgData.DataSource = dt;
+            this.dgData.DataBind();
+        }
+
+
+        private void ExecuteMYSQL(string strDataSource, string strUsername, string strPassword, string strSQL)
+        {
+            var strDSNTemplate = "server={0}; uid={1}; pwd={2};";
+            var sqlConnection = string.Format(strDSNTemplate, strDataSource, strUsername, strPassword);
+            var adapter = new MySqlDataAdapter(strSQL, sqlConnection);
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet, "sql");
+            var dt = dataSet.Tables["sql"];
+            this.dgData.DataSource = dt;
+            this.dgData.DataBind();
+        }
+    }
+}
