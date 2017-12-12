8dcf88e008a3b9c105bb20195c8800622c63bbe1
Fiach Reid <fiach.reid@gmail.com>
2017-12-12 15:36:09 +0000

    added json output

---
+++ api.aspx.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MobileSQL
+{
+    public partial class api : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var strDataSource = Request.QueryString["DataSource"];
+                var strUsername = Request.QueryString["Username"];
+                var strPassword = Request.QueryString["Password"];
+                var strSQL = Request.QueryString["SQL"];
+                var strDB = Request.QueryString["DB"].ToLower();
+                var dt = new DataTable();
+                if (strDB == "sql")
+                {
+                    dt = ExecuteMSSQL(strDataSource, strUsername, strPassword, strSQL);
+                }
+                else
+                {
+                    dt = ExecuteMYSQL(strDataSource, strUsername, strPassword, strSQL);
+                }
+                string result = JsonConvert.SerializeObject(dt, Newtonsoft.Json.Formatting.Indented);
+                Response.Write(result);
+            }
+            catch(Exception ex)
+            {
+                string result = JsonConvert.SerializeObject(ex, Newtonsoft.Json.Formatting.Indented);
+                Response.Write(result);
+            }
+        }
+
+
+        private DataTable ExecuteMSSQL(string strDataSource, string strUsername, string strPassword, string strSQL)
+        {
+            var strDSNTemplate = "Network Library=DBMSSOCN;Data Source={0}; User ID={1}; Password={2};";
+            var sqlConnection = string.Format(strDSNTemplate, strDataSource, strUsername, strPassword);
+            var adapter = new SqlDataAdapter(strSQL, sqlConnection);
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet, "sql");
+            var dt = dataSet.Tables["sql"];
+            return dt;
+        }
+
+
+        private DataTable ExecuteMYSQL(string strDataSource, string strUsername, string strPassword, string strSQL)
+        {
+            var strDSNTemplate = "server={0}; uid={1}; pwd={2};";
+            var sqlConnection = string.Format(strDSNTemplate, strDataSource, strUsername, strPassword);
+            var adapter = new MySqlDataAdapter(strSQL, sqlConnection);
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet, "sql");
+            var dt = dataSet.Tables["sql"];
+            return dt;
+        }
+    }
+}
